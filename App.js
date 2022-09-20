e7ff534e876c91536cd362f5939ccda2493edd45
kacperkapusciak <kacper.kapusciak@swmansion.com>
2022-09-20 10:03:40 +0200

    Setup @react-navigation/drawer

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
+
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
+
+function Feed(){
+  return (
+    <View>
+      <Text>Test Feed View</Text>
+    </View>
+  )
+}
+
+function Comment(){
+  return (
+    <View>
+      <Text>Test Comment View</Text>
+    </View>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator>
+         <Drawer.Screen name="Feed" component={Feed} />
+         <Drawer.Screen name="Comment" component={Comment} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
